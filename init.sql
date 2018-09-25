23f238346f52dd11bc7f0938953713d1ac93d186
Elmer Sevillano <esevillano@ymail.com>
2018-09-24 17:09:19 -0700

    Add Test post for Forum 3

--- init.sql
+++ init.sql
@@ -68,15 +68,16 @@ INSERT INTO Threads
 VALUES
   (1, 'Thread Test 1'), --id=1
   (1, 'Thread Test 1.2'), --id=2
-  (2, 'Thread Test 2'), --id=3
-  (3, 'Thread Test 3'); --id=4
+  (2, 'Thread Test 2 help'), --id=3
+  (3, 'Thread Test 3 is here'); --id=4
 
 INSERT INTO Posts
   (`AuthorId`, `ThreadBelongsTo`, `PostsTimestamp`, `Message`)
 VALUES
   (1, 1, 'Tue, 02 Sep 2018 15:42:28 GMT', 'Post Test - Author=1 Thread=1'), --id=1
-  (2, 1, 'Wed, 03 Sep 2018 15:43:28 GMT', 'Post Test - Author=2 Thread=1'), --id=2
+  (2, 1, 'Wed, 03 Sep 2018 15:43:28 GMT', 'Post Test - Author=2 Thread=1 Most Recent'), --id=2
   (3, 2, 'Thu, 04 Sep 2018 15:42:28 GMT', 'Post Test - Author=3 Thread=1'), --id=3
-  (1, 2, 'Fri, 05 Sep 2018 15:43:28 GMT', 'Post Test - Author=1 Thread=1'), --id=4
+  (1, 2, 'Fri, 05 Sep 2018 15:43:28 GMT', 'Post Test - Author=1 Thread=1 Most Recent'), --id=4
   (3, 3, 'Tue, 06 Sep 2018 15:42:28 GMT', 'Post Test - Author=3 Thread=2'), --id=5
-  (2, 3, 'Wed, 07 Sep 2018 15:43:28 GMT', 'Post Test - Author=2 Thread=2'); --id=6
+  (2, 3, 'Wed, 07 Sep 2018 15:43:28 GMT', 'Post Test - Author=2 Thread=2 Most Recent'), --id=6
+  (2, 4, 'Wed, 07 Sep 2018 15:45:28 GMT', 'Post TEST 4 EXISTS') --id=7
